f6e0172c10f25287a787d73a9db9b15a1a724d28
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2021-12-16 10:22:14 -0600

    set up index html and beginning divs, began css styling for paddles and ball

---
+++ README.md
@@ -0,0 +1,3 @@
+Hello!
+
+To see the wireframe for this project, please go to: https://www.figma.com/file/EfFN7MWXpZZ3QhGB2S5oaF/ping-pong?node-id=0%3A1 .
--- pong-game/js/script.js
+++ pong-game/js/script.js
@@ -1,1 +1,5 @@
+// =================== DOM QUERIES UP HERE ======================
+
 console.log("Ready to play?")
+
+// ===============  ADD NAMES FOR PLAYER 1 AND PLAYER 2 =========
