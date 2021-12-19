3c46170864013f6c985b8b41b57e75254a9e84e6
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2021-12-19 12:34:27 -0600

    added canvas dimensions

--- pong-game/js/script.js
+++ pong-game/js/script.js
@@ -1,6 +1,13 @@
 // =================== DOM QUERIES UP HERE ======================
 
+
+
 console.log("Ready to play?")
 
 // ===============  ADD NAMES FOR PLAYER 1 AND PLAYER 2? =========
 
+
+
+// element.getBoundingClientRect will help for ball movement
+
+// =============== EXPERIMENTING WITH NEW FUNCTIONS HERE =========
