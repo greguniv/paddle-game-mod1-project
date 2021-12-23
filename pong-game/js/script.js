7e00cfbffaf82c47bc1732912a502e1c3850bed4
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2021-12-23 12:45:52 -0600

    added function in js to track mouse movement and drew the ball in js onto the canvas

--- pong-game/js/script.js
+++ pong-game/js/script.js
@@ -4,10 +4,40 @@
 
 console.log("Ready to play?")
 
-// ===============  ADD NAMES FOR PLAYER 1 AND PLAYER 2? =========
+// =============== CANVAS =========
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+let c = canvas.getContext('2d');
 
+// ================== JS FOR THE BALL ==========================
 
-// element.getBoundingClientRect will help for ball movement
+//this creates the ball
+c.beginPath();
+c.arc(300, 300, 15, 0, Math.PI * 2, false);
+c.strokeStyle = "blue";
+c.stroke();
+c.fillStyle = "blue";
+c.fill();
 
-// =============== EXPERIMENTING WITH NEW FUNCTIONS HERE =========
+//this js is to have it bounce around the canvas
+function animate() {
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+// ============== THIS IS THE JS FOR THE INTERACTION FOR THE BALL ============
+
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+//this js below tracks where the mouse is on the canvas
+window.addEventListener('mousemove',
+    function(event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+        console.log(mouse)
+    })
