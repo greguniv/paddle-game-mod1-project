53cf43ca2a9adbb5a85cad5da5c05096c27b0201
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2021-12-29 12:33:20 -0600

    beginning score & collosion js

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Hello!
 
+To PLAY the game created here, please go to: [url will go here]
+
 To see the wireframe for this project, please refer to: https://www.figma.com/file/EfFN7MWXpZZ3QhGB2S5oaF/ping-pong?node-id=0%3A1 .
 
 Tutorials used for reference and guidance (in order of being found) include:
@@ -9,3 +11,31 @@ Tutorials used for reference and guidance (in order of being found) include:
 
 Sounds from:
 - https://mixkit.co/free-sound-effects/game/
+
+The initial idea to execute the game was via divs - to avoid using 'canvas'. However, upon introduction to the videos by Chris Courses on YouTube, this idea was scrapped for using the canvas, as a better understanding was given and it appeared more streamlined.
+
+Project Details
+
+*Mandatory To Pass:
+MVP - Minimum Viable Product
+Your game must meet these requirements:
+
+Built with HTML, CSS, JavaScript, and the DOM.
+Deployed on Netlify, GitHub Pages or any other hosting service.
+Commits to GitHub every day. 
+A README.md  with explanations of the technologies used, the approach taken, a link to your live site, installation instructions, unsolved problems, etc.
+
+Game must have:
+Must be a two player game (either against the computer or against another player)
+A win state - a way for the player to win the game
+High score can be considered a win state
+A lose state - a way for the player to lose the game
+A way to keep playing if the game is not over.
+Multiple rounds to play - a round must begin, end, and there must be a way to check if the game should continue or the overall game is won or lost
+
+
+*Stretch Goals (Not Mandatory):
+A way to reset the board and play again
+CSS to give your game a personal and fun style
+Responsive mobile design
+Work with your instructor to determine additional stretch goals
--- pong-game/js/script.js
+++ pong-game/js/script.js
@@ -3,12 +3,15 @@
 console.log("Ready to play?")
 
 // =============================== CANVAS ========================
-const canvas = document.querySelector('canvas');
+let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // this lets the entire webpage be our canvas
-const c = canvas.getContext('2d');
+let c = canvas.getContext('2d');
 //to draw anything on canvas, we need to getContext
+
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
 
 // ======================= SOUNDS LIVE HERE =======================
 const hitSound = new Audio('./game-sounds/ball-hitting-paddle-sound.wav');
@@ -19,53 +22,117 @@ const winnerSound = new Audio('./game-sounds/you-won-sound.wav');
 // ================== JS FOR THE BALL ===========================
 
 
-//this creates the ball
-let radius = 15;
-
-function Circle(x, y, dx, dy, radius) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
-
-    this.draw = function () {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'black';
-        c.stroke();
-        c.fillStyle = 'black';
-        c.fill();
-    }
-
-    this.update = function () {
-        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-            this.dx = -this.dx;
-        }
-
-        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-            this.dy = -this.dy;
-        }
-
-        this.x += this.dx;
-        this.y += this.dy;
-
-        this.draw();
-    }
+//this sets up how the ball appears
+let ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2, //this x & y center the ball on the canvas
+    radius: 10,
+    speed: 5,
+    dx: 5,
+    dy: 5,
+    color: 'black',
 }
 
-console.log()
+//this function is for actually making the ball appear on the canvas
+function drawBall(x, y, radius, color) {
+    x.fillStyle = color;
+    c.beginPath();
+    c.arc(x, y, radius, 0, Math.PI * 2, true);
+    c.closePath();
+    x.fill();
+}
 
-// ================ THIS IS THE JS FOR THE PADDLES ==================
 
+//this is the movement of the ball
+
+//this RESETS the ball when it hits the canvas/the edge
+function reset() {
+    ball.x = canvas.width / 2; //the middle of the canvas along x-axis
+    ball.y = canvas.height / 2; //middle along the canvas' y-axis
+    ball.speed = 7;
+
+    ball.dx = -ball.dx; //reverses the direction
+    ball.dy = -ball.dy;
+}
+
+
+// ========================= JS FOR THE PADDLES ================
 const paddleWidth = 10;
 const paddleHeight = 100;
+
+let upArrowPressed = false;
+let downArrowPressed = false;
 
 const playerOne = {
     x: 10,
     y: canvas.height / 2 - paddleHeight / 2,
     width: paddleWidth,
     height: paddleHeight,
-    color: 'pink',
-    score: 0
+    color: 'black',
+    score: 0,
 };
+
+const comp = {
+    x: canvas.width - (paddleWidth + 10), //sets up the comp paddle across the player Paddle
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: paddleWidth,
+    height: paddleHeight,
+    color: 'black',
+    score: 0,
+};
+// console.log(playerOne) - sanity check
+// console.log(comp) - sanity check
+
+//this is the Paddle function
+function drawPaddle(x, y, width, height, color) {
+    c.fillStyle = color;
+    c.fillRect(x, y, width, height);
+}
+
+//this function is for the EventListeners - trigger to move the Player Paddle!!! - currently set up for Up & Down arrow key
+// -STRETCH GOAL: use mouse to move the Paddles -
+
+function keyDown(event) { //activated when key is pressed
+    switch(event.code) {
+        case "ArrowUp":
+            upArrowPressed = true;
+            break;
+        case "ArrowDown":
+            downArrowPressed = true;
+            break;
+    }
+}
+
+function keyUp(event) { //activates when the key is released/let go/no longer pressed
+    switch(event.code) {
+        case "ArrowUp":
+            upArrowPressed = false;
+            break;
+        case "ArrowDown":
+            downArrowPressed = false;
+            break;
+    }
+}
+
+
+//========================== GAME LOOP ========================
+
+//this will be the Scoring function
+
+function Score(x, y, score){
+    c.fillStyle = 'black';
+    c.font = '35px sans-serif';
+}
+
+
+// this detects the COLLISION of the ball to the paddles
+function getDistance() {
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
+
+
+
+//this will LOOP the game 
